7009c5eb89ee10054f01f26f94f6e4c0751fa1b9
Davide Bertolotto <davide.bertolotto@gmail.com>
2019-02-12 10:59:39 +0100

    various fixes + missing certificate

--- filebeat/build.sh
+++ filebeat/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t dbertolotto/filebeat:6.6.0
+docker build -t dbertolotto/filebeat:6.6.0 .
--- python-log-generator/build.sh
+++ python-log-generator/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t dbertolotto/python-log-generator:latest
+docker build -t dbertolotto/python-log-generator:latest .
